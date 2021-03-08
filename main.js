64cfb06dd57b04f8a36a5976c3b4c08c31448e4b
Onmae <37141155+Onmae@users.noreply.github.com>
2021-03-08 17:21:38 +0900

    JS : project filter, button state

--- main.js
+++ main.js
@@ -46,6 +46,37 @@ arrowUp.addEventListener('click', () =>{
     scrollIntoView('#home');
 });
 
+const workBtnContainer = document.querySelector('.work__category');
+const projectsContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+
+    //Remove selection and set new
+    const active = document.querySelector('.category__btn.active');
+    active.classList.remove('active');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('active');
+
+    projectsContainer.classList.add('animate-out');
+    setTimeout(()=> {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectsContainer.classList.remove('animate-out');
+    },300);
+
+
+});
+
+
 function scrollIntoView(selecctor) {
     const scrollTo = document.querySelector(selecctor);
     scrollTo.scrollIntoView( {behavior: 'smooth'});
